2a6b1d9579d78213d50da0f5c0e64b123abea464
Iswarya76 <babumohan394@gmail.com>
2023-07-16 22:33:33 +0530

    Day 32 Task completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import "./CSS/style.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Book_List from './Book_List';
+import Login from './Login';
+import PortalLayout from './PortalLayout';
+import Add_Book from './Add_Book';
+import Edit_Book from './Edit_Book';
+import Issue_Form from './Issue_Form';
+import Issue_List from './Issue_List';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="" element={<Login/>} />
+      <Route path="/portal" element={<PortalLayout/>}>
+        <Route path="dashboard" element={<Dashboard/>} />
+        <Route path="book_list" element={<Book_List />} />
+        <Route path="book_add" element={<Add_Book />} />
+        <Route path="book_edit/:id" element={<Edit_Book />} />
+        <Route path="book_issue" element={<Issue_Form />} />
+        <Route path="issue_list" element={<Issue_List />} />
+      </Route>
+    </Routes>      
+  </BrowserRouter>
   );
 }
 
